dd4fbd9d82e793dd22646028e8bdbabe59b5fe5e
Abracazebra <alexbx@yandex.ru>
2014-05-28 11:57:27 -0400

    ...

---
+++ Assets/Levels/Plateformer/OldMovementSystem/pressNothingActionOld.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pressNothingAction : AbsAction {
+	public pressNothingAction(GameObject pPlayer, PlayerState pState) : base(pPlayer, pState){
+	}
+	
+	public override bool execute (int duration)
+	{
+		if(curTime < duration){
+			if(state.isOnGround){
+				state.velocity.x = 0;
+			}
+			state.adjustmentVelocity.x = 0;
+			curTime++;
+			if(curTime == duration){
+				curTime = 0;
+				return true;
+			}
+			return false;
+			
+		}
+		else{
+			curTime = 0;
+			return true;
+		}
+	}
+	
+	
+}
